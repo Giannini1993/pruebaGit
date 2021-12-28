ee8705a93ec6a586be41ae56d142e12e5c1ae5b1
MatiBalbiDev <matiasbalbi96@gmail.com>
2021-12-28 00:08:21 -0300

    Agregue un comentario

--- PruebaGit/src/main/java/com/example/demo/PruebaGitApplication.java
+++ PruebaGit/src/main/java/com/example/demo/PruebaGitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PruebaGitApplication {
 	
-	//Esto es un comentario. 
+	//Esto es un comentario..
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaGitApplication.class, args);
 	}
